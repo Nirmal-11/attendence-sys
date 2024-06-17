26626a95c3c3c6e195b76478858a3c28666d71e8
nirmal <nirmalroka156@gmail.com>
2024-06-17 14:00:08 +0545

    env for prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,23 @@
-/** @type {import('next').NextConfig} */
+import dotenv from 'dotenv';
+
+// Load environment variables from .env.local for development
+dotenv.config();
+
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol: "https",
-                hostname: "images.pexels.com"
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.pexels.com",
+      },
+    ],
+  },
+  env: {
+    MONGO_URL_DEV: process.env.MONGO_URL_DEV,
+    MONGO_URL_PROD: process.env.MONGO_URL_PROD,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+  },
 };
 
 export default nextConfig;
