e3fdd532c7b1c35ce8e2a8fca456dc11754c3b8c
nirmal <nirmalroka156@gmail.com>
2024-06-17 17:34:12 +0545

    sidenav updated

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -17,7 +17,7 @@ export default function SideNav() {
     if (status !== 'loading') {
       setIsLoading(false);
     }
-  }, [status]);
+  }, [session, status]);
 
   const handleSignOut = () => {
     toast.success('You have been logged out!!', { position: "top-center", theme: "dark" });
