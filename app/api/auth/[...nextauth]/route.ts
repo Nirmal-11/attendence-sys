cb23e3f6580b6e59d5232ef5e47dd94934fc0379
nirmal <nirmalroka156@gmail.com>
2024-06-18 10:12:44 +0545

    dupdated

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,61 +1,56 @@
-import NextAuth, {AuthOptions } from "next-auth"
-import { Account, User as AuthUser } from "next-auth";
-// import GithubProvider from "next-auth/providers/github"
+import NextAuth, { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import User from "@/app/lib/models"
+import User from "@/app/lib/models";
 import connect from "@/app/lib/utils";
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs';
 
- const authOptions: AuthOptions = {
-    providers: [
-        CredentialsProvider({
-            id: "credentials",
-            name: "Credentials",
-            credentials: {
-                email: { label: "Email", type: "text" },
-                password: { label: "password", type: "password" },
-            },
-            async authorize(credentials: any) {
-                await connect();
-                try {
-                    const user = await User.findOne({ email: credentials.email});
-                    
-                    if (user) {
-                        const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
-                        if(isPasswordCorrect){
-                            return user;
-                        }
-                    }
-                } catch (error: any) {
-                    throw new Error(error)
-                }
-            }
-        }),
-    ],
-    callbacks: {
-        // async signIn({ user, account }: { user: AuthUser; account: Account }) {
-        //   if (account?.provider == "credentials") {
-        //     return true;
-        //   }
-        // },
-        async jwt({ token, user }: any) {
-            // If the user object is available and has an ID, add it to the token
-            if (user?.id) {
-              token.id = user.id;
-              token.isAdmin = user.isAdmin;
-
-            }
-            return token;
-          },
-          async session({ session, token }: any) {
-            // Assign the user ID from the token to the session object
-            session.user.id = token.id;
-            session.user.isAdmin = token.isAdmin;
-            return session;
-          },
-        
+const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "password", type: "password" },
       },
-}
+      async authorize(credentials: any) {
+        await connect();
+        try {
+          const user = await User.findOne({ email: credentials.email });
 
- const handler = NextAuth(authOptions);
-export {handler as GET, handler as POST}
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
+            if (isPasswordCorrect) {
+              return user;
+            } else {
+              throw new Error('Invalid password');
+            }
+          } else {
+            throw new Error('No user found with the provided email');
+          }
+        } catch (error: any) {
+          console.error('Error during authorization:', error);
+          throw new Error('Error during authorization');
+        }
+      }
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }: any) {
+      if (user?.id) {
+        token.id = user.id;
+        token.isAdmin = user.isAdmin;
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      session.user.id = token.id;
+      session.user.isAdmin = token.isAdmin;
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
