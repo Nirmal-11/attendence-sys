9686acbc0940eb1cdd940b991fdd42d3fee98265
nirmal <nirmalroka156@gmail.com>
2024-06-18 13:13:28 +0545

    asd

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -49,11 +49,12 @@ export default function SideNav() {
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <div>
-          { session ? (
+        {status === 'loading' && <p>Loading...</p>}
+        {session && (
             <button onClick={handleSignOut} className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
               <PowerIcon className="w-6" /> Sign Out
             </button>
-          ) : null}
+          )}
         </div>
       </div>
     </div>
