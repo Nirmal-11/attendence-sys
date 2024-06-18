aff5bb2d9b5861e4fe1bf3ef5eb129441504ba69
nirmal <nirmalroka156@gmail.com>
2024-06-18 14:06:52 +0545

    :asd

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,31 +2,38 @@
 import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import { PowerIcon } from '@heroicons/react/24/outline';
-import { signOut, useSession } from "next-auth/react";
+import { useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react'
 import { toast } from 'react-toastify';
 import { useEffect, useState } from 'react';
 
 export default function SideNav() {
   const { data: session, status } = useSession();
   const [isLoading, setIsLoading] = useState(true);
+
   
 
   useEffect(() => {
-    console.log("Session status:", status);
+    console.log("Session status 1:", status);
     if (status !== 'loading') {
       setIsLoading(false);
     }
   }, [status]);
-  
+
+
   useEffect(() => {
-    console.log("UserSession data:", session);
+    console.log("UserSession data 1:", session);
     if (session) {
       console.log("User is authenticated");
     } else {
       console.log("User is not authenticated");
     }
   }, [session]);
-  
+
+  useEffect(() => {
+    console.log("Session status 2:", status);
+    console.log("User session data 2:", session);
+  }, [status, session]);
 
   const handleSignOut = () => {
     toast.success('You have been logged out!!', { position: "top-center", theme: "dark" });
@@ -50,8 +57,8 @@ export default function SideNav() {
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <div>
-        {status === 'loading' && <p>Loading...</p>}
-        {session && (
+          {status === 'loading' && <p>Loading...</p>}
+          {session && (
             <button onClick={handleSignOut} className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
               <PowerIcon className="w-6" /> Sign Out
             </button>
